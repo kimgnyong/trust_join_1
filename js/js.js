c27ec49dae7887cb1d74d198e158d5afa1a336d5
kimgnyong <rlarmsfdyd@naver.com>
2021-07-05 10:09:37 +0900

    Upload 2021-07-05 10:09

---
+++ js/js.js
@@ -0,0 +1,15 @@
+;(function($){
+
+    var tema = {
+       
+        init: function(){
+            this.section1Fn();
+        },
+        section1Fn: function(){
+        }
+    }
+
+
+    tema.init();
+
+})(jQuery);
