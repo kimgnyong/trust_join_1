5185a9c1c636989f9eaf7afae9843f7b6d31c932
kimgnyong <rlarmsfdyd@naver.com>
2021-07-05 18:59:44 +0900

    updatae 2021-07-05

--- js/js.js
+++ js/js.js
@@ -6,6 +6,13 @@
             this.section1Fn();
         },
         section1Fn: function(){
+            var $textP = $('#wrap .wrap .gap .content p')
+
+            $textP.on({
+                click:function(){
+                    alert('클릭되었습니다.');
+                }
+            })
         }
     }
 
